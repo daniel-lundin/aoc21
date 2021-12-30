28a42fdebb99ecde3745f797c82e74361a6fadec
Daniel Lundin <d-dog@d-dog.se>
2021-12-30 21:27:06 +0100

    23 part 1

--- 23.go
+++ 23.go
@@ -2,7 +2,8 @@ package main
 
 import (
 	"fmt"
-	"time"
+	"math"
+	"os"
 )
 
 type Ampiphode struct {
@@ -29,71 +30,104 @@ func TwentyThree() {
 		{8, 1}: "D",
 		{8, 2}: "A",
 	}
+	// ampiphodes := map[Coord2D]string{
+	// 	{2, 1}: "A",
+	// 	{2, 2}: "D",
+	// 	{2, 3}: "D",
+	// 	{2, 4}: "D",
+	// 	{4, 1}: "C",
+	// 	{4, 2}: "C",
+	// 	{4, 3}: "B",
+	// 	{4, 4}: "D",
+	// 	{6, 1}: "B",
+	// 	{6, 2}: "B",
+	// 	{6, 3}: "A",
+	// 	{6, 4}: "B",
+	// 	{8, 1}: "A",
+	// 	{8, 2}: "A",
+	// 	{8, 3}: "C",
+	// 	{8, 4}: "C",
+	// }
 
 	moves := possibleMoves(ampiphodes)
 
-	highestCost := 0
-	makeMove(moves, ampiphodes, 0, &highestCost)
+	lowestCost := math.MaxInt
+	makeMove(moves, ampiphodes, 0, &lowestCost)
+
+	fmt.Printf("lowest cost %d\n", lowestCost)
 }
 
-func makeMove(moves [][]Coord2D, ampis map[Coord2D]string, cost int, highestCost *int) {
+func makeMove(moves [][]Coord2D, ampis map[Coord2D]string, cost int, lowestCost *int) {
 
 	for _, move := range moves {
-		clonedAmbis := cloneAmpis(ampis)
-		printAmbis(ampis)
-		fmt.Printf("moving %v\n", move)
-		ampi := clonedAmbis[move[0]]
-		clonedAmbis[move[0]] = ""
-		clonedAmbis[move[1]] = ampi
-		time.Sleep(500 * time.Millisecond)
-		fmt.Printf("after move:\n")
-		printAmbis(clonedAmbis)
-		newMoves := possibleMoves(clonedAmbis)
+		clonedAmpis := cloneAmpis(ampis)
+		//fmt.Println()
+		//printAmbis(ampis)
+		//fmt.Printf("moving %v\n", move)
+		ampi := clonedAmpis[move[0]]
+		clonedAmpis[move[0]] = ""
+		clonedAmpis[move[1]] = ampi
+		//fmt.Printf("after move:\n")
+		//printAmbis(clonedAmpis)
+		//fmt.Println()
+		//time.Sleep(500 * time.Millisecond)
+		newMoves := possibleMoves(clonedAmpis)
 
+		moveCost := (intAbs(move[0].x-move[1].x) + move[0].y + move[1].y) * getMoveCost(ampis[move[0]])
+
+		if moveCost+cost > *lowestCost {
+			continue
+		}
 		if len(newMoves) > 0 {
-			makeMove(newMoves, clonedAmbis, cost+1, highestCost)
+			makeMove(newMoves, clonedAmpis, cost+moveCost, lowestCost)
+		} else {
+			// fmt.Printf("no more moves\n ")
+			// printAmbis(clonedAmpis)
+			if allInPlace(clonedAmpis) {
+				*lowestCost = minInt(cost+moveCost, *lowestCost)
+				//fmt.Printf("good posidition %d", cost+moveCost)
+				// printAmbis(clonedAmpis)
+			}
 		}
 	}
 }
 
-func printAmbis(ampis map[Coord2D]string) {
-	fmt.Println("#############")
-	fmt.Printf("#")
-	for x := 0; x < 11; x++ {
-		ambi := ampis[Coord2D{x, 0}]
-		if ambi != "" {
-			fmt.Printf(ambi)
-		} else {
-			fmt.Printf(".")
+func allInPlace(ampis map[Coord2D]string) bool {
+	for coord, ampi := range ampis {
+		if ampi == "" {
+			continue
 		}
-	}
-	fmt.Printf("#\n")
-	fmt.Printf("###")
-	for x := 2; x < 8; x++ {
-		ambi := ampis[Coord2D{x, 1}]
-		if ambi != "" {
-			fmt.Printf(ambi)
-		} else if x%2 == 0 {
-			fmt.Printf(".")
-		} else {
-			fmt.Printf("#")
+		if coord.x != sideroomX(ampi) {
+			return false
 		}
-	}
-	fmt.Printf("###\n")
 
-	fmt.Printf("  #")
-	for x := 2; x < 8; x++ {
-		ambi := ampis[Coord2D{x, 2}]
-		if ambi != "" {
-			fmt.Printf(ambi)
-		} else if x%2 == 0 {
-			fmt.Printf(".")
-		} else {
-			fmt.Printf("#")
-		}
 	}
-	fmt.Printf("#\n")
-	fmt.Println("  #########  ")
+	return true
+}
+
+func intAbs(a int) int {
+	if a < 0 {
+		return -1 * a
+	}
+	return a
+}
+
+func getMoveCost(ampi string) int {
+	if ampi == "A" {
+		return 1
+	}
+	if ampi == "B" {
+		return 10
+	}
+	if ampi == "C" {
+		return 100
+	}
+	if ampi == "D" {
+		return 1000
+	}
+	fmt.Printf("Unknown ampi %s\n", ampi)
+	os.Exit(1)
+	return -1
 }
 
 func cloneAmpis(ampis map[Coord2D]string) map[Coord2D]string {
@@ -107,6 +141,9 @@ func cloneAmpis(ampis map[Coord2D]string) map[Coord2D]string {
 func possibleMoves(ampis map[Coord2D]string) [][]Coord2D {
 	moves := make([][]Coord2D, 0)
 	for coord, ampi := range ampis {
+		if ampi == "" {
+			continue
+		}
 		// fmt.Printf("Checking possible moves for %s %v\n", ampi, coord)
 		freeXSpots := hallwaySpots(coord.x, ampis)
 		desinationX := sideroomX(ampi)
@@ -124,7 +161,7 @@ func possibleMoves(ampis map[Coord2D]string) [][]Coord2D {
 
 			// Final destination possible
 			if possibleDesination != -1 {
-				if freePassage(coord.x, desinationX, freeXSpots) {
+				if freePassage(coord.x, desinationX, ampis) {
 					// fmt.Printf("found move %v\n", []Coord2D{coord, {desinationX, possibleDesination}})
 					moves = append(moves, []Coord2D{coord, {desinationX, possibleDesination}})
 					continue
@@ -139,9 +176,13 @@ func possibleMoves(ampis map[Coord2D]string) [][]Coord2D {
 		}
 
 		if coord.y == 1 {
+			// Correct place
+			if coord.x == sideroomX(ampi) && ampis[Coord2D{coord.x, 2}] == ampi {
+				continue
+			}
 			// Find hallways spots and final destinations
 			if possibleDesination != -1 {
-				if freePassage(coord.x, desinationX, freeXSpots) {
+				if freePassage(coord.x, desinationX, ampis) {
 					// fmt.Printf("found move %v\n", []Coord2D{coord, {desinationX, possibleDesination}})
 					moves = append(moves, []Coord2D{coord, {desinationX, possibleDesination}})
 					continue
@@ -156,7 +197,7 @@ func possibleMoves(ampis map[Coord2D]string) [][]Coord2D {
 
 		if coord.y == 0 {
 			if possibleDesination != -1 {
-				if freePassage(coord.x, desinationX, freeXSpots) {
+				if freePassage(coord.x, desinationX, ampis) {
 					// fmt.Printf("found move %v\n", []Coord2D{coord, {desinationX, possibleDesination}})
 					moves = append(moves, []Coord2D{coord, {desinationX, possibleDesination}})
 					continue
@@ -167,17 +208,14 @@ func possibleMoves(ampis map[Coord2D]string) [][]Coord2D {
 	return moves
 }
 
-func freePassage(fromX int, toX int, freeXSpots []int) bool {
+func freePassage(fromX int, toX int, ampis map[Coord2D]string) bool {
 	minX := minInt(fromX, toX)
 	maxX := maxInt(fromX, toX)
 	for x := minX; x < maxX; x++ {
-		free := false
-		for _, freeX := range freeXSpots {
-			if freeX == x {
-				free = true
-			}
+		if x == fromX {
+			continue
 		}
-		if !free {
+		if ampis[Coord2D{x, 0}] != "" {
 			return false
 		}
 	}
@@ -205,11 +243,11 @@ func sideroomEmpty(x int, ampis map[Coord2D]string) bool {
 }
 
 func freeDestinations(ampi string, ampis map[Coord2D]string) int {
-	desinationX := sideroomX(ampi)
-	if sideroomEmpty(desinationX, ampis) {
+	destinationX := sideroomX(ampi)
+	if sideroomEmpty(destinationX, ampis) {
 		return 2
 	}
-	if ampis[Coord2D{desinationX, 2}] == ampi && ampis[Coord2D{desinationX, 1}] == "" {
+	if ampis[Coord2D{destinationX, 2}] == ampi && ampis[Coord2D{destinationX, 1}] == "" {
 		return 1
 	}
 	return -1
@@ -218,23 +256,67 @@ func freeDestinations(ampi string, ampis map[Coord2D]string) int {
 func hallwaySpots(startX int, ampis map[Coord2D]string) []int {
 	possibleSpots := make([]int, 0)
 	// left
-	for x := startX; x >= 0; x-- {
+	for x := startX - 1; x >= 0; x-- {
 		if ampis[Coord2D{x, 0}] == "" {
 			// Not outside doors
 			if x != 2 && x != 4 && x != 6 && x != 8 {
 				possibleSpots = append(possibleSpots, x)
 			}
+		} else {
+			break
 		}
 	}
 
 	// right
-	for x := startX; x <= 10; x++ {
+	for x := startX + 1; x <= 10; x++ {
 		if ampis[Coord2D{x, 0}] == "" {
 			// Not outside doors
 			if x != 2 && x != 4 && x != 6 && x != 8 {
 				possibleSpots = append(possibleSpots, x)
 			}
+		} else {
+			break
 		}
 	}
 	return possibleSpots
+}
+
+func printAmbis(ampis map[Coord2D]string) {
+	fmt.Println("#############")
+	fmt.Printf("#")
+	for x := 0; x < 11; x++ {
+		ambi := ampis[Coord2D{x, 0}]
+		if ambi != "" {
+			fmt.Printf(ambi)
+		} else {
+			fmt.Printf(".")
+		}
+	}
+	fmt.Printf("#\n")
+	fmt.Printf("###")
+	for x := 2; x <= 8; x++ {
+		ambi := ampis[Coord2D{x, 1}]
+		if ambi != "" {
+			fmt.Printf(ambi)
+		} else if x%2 == 0 {
+			fmt.Printf(".")
+		} else {
+			fmt.Printf("#")
+		}
+	}
+	fmt.Printf("###\n")
+
+	fmt.Printf("  #")
+	for x := 2; x <= 8; x++ {
+		ambi := ampis[Coord2D{x, 2}]
+		if ambi != "" {
+			fmt.Printf(ambi)
+		} else if x%2 == 0 {
+			fmt.Printf(".")
+		} else {
+			fmt.Printf("#")
+		}
+	}
+	fmt.Printf("#\n")
+	fmt.Println("  #########  ")
 }
